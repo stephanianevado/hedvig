9698194b0a68ebfe643295bcb3e327af4ed0d667
Stephania Nevado <stephania.arantxa@gmail.com>
2024-01-07 18:02:15 +0100

    configure env file

--- next.config.js
+++ next.config.js
@@ -2,11 +2,13 @@
 
 const { PHASE_PRODUCTION_SERVER } = require('next/constants')
 
+const APP_BASE_URL = process.env.APP_BASE_URL
+
 // This is a workaround to be able to seed the database on server startup.
 // Next.js unfortunately doesn't have a great way to run logic once on startup.
 // In a real world scenario I would have used a completely separate backend for database communication and API.
 function seedDatabase() {
-  fetch('http://localhost:3000/api/seed', { method: 'POST' })
+  fetch(`${APP_BASE_URL}/api/seed`, { method: 'POST' })
 }
 
 module.exports = (phase, { defaultConfig }) => {
